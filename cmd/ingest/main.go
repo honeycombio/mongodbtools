efd021f9ae239283edcfa184778d7483e4f5a825
Chris Toshok <toshok@hound.sh>
2016-10-26 16:39:13 -0700

    remove unneeded code

--- cmd/ingest/main.go
+++ cmd/ingest/main.go
@@ -10,21 +10,6 @@ import (
 	"github.com/honeycombio/mongodbtools/logparser"
 )
 
-const (
-	ctimeTimeFormat        = "Mon Jan _2 15:04:05.000"
-	ctimeNoMSTimeFormat    = "Mon Jan _2 15:04:05"
-	iso8601UTCTimeFormat   = "2006-01-02T15:04:05Z"
-	iso8601LocalTimeFormat = "2006-01-02T15:04:05.999999999-0700"
-
-	timestampFieldName  = "timestamp"
-	namespaceFieldName  = "namespace"
-	databaseFieldName   = "database"
-	collectionFieldName = "collection"
-	locksFieldName      = "locks"
-)
-
-var timestampFormats = []string{iso8601LocalTimeFormat, iso8601UTCTimeFormat, ctimeNoMSTimeFormat, ctimeTimeFormat}
-
 func main() {
 	file, err := os.Open(os.Args[1])
 	if err != nil {
